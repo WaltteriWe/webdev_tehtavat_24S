9a64b83e201299b95bd43754edf534d7d0c26802
Waltteri <waltteri.westerholm@gmail.com>
2024-08-23 21:45:15 +0300

    tehtävistö2 done

---
+++ js_tehtavat_2/t4/t4.js
@@ -0,0 +1,9 @@
+function sortarray(numbers) {
+    return numbers.sort((a, b) => a - b);
+}
+
+const numbers = [6, 7, 4, 3, 1, 6];
+console.log("Alkuperäinen taulukko: ", numbers);
+
+const sortedNumbers = sortarray(numbers);
+console.log("Järjestetty taulukko: ", sortedNumbers);
