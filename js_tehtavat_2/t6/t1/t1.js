5284969bb857013562d503ff5d32220deb76e736
Waltteri <waltteri.westerholm@gmail.com>
2024-08-23 21:27:58 +0300

    t1 tehty tehtävistöstä 2

---
+++ js_tehtavat_2/t6/t1/t1.js
@@ -0,0 +1,15 @@
+let fruits = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[2]);
+console.log(fruits[fruits.length - 1]);
+
+let vegetables = [];
+vege = prompt('Anna vihanneksia (3 eri): ');
+vegetables.push(vege);
+vege = prompt('Anna vihanneksia (vielä 2): ');
+vegetables.push(vege);
+vege = prompt('Anna vihanneksia (vielä 1 :D): ');
+vegetables.push(vege);
+console.log(vegetables);
+console.log(vegetables.length);
